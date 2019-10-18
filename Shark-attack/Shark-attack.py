c0e25a45d305e5e38d85c336dd5b0b2c6f062cf2
UNIpope <jack.dugg.ch@gmail.com>
2019-10-18 12:36:17 +0100

    oo

--- Shark-attack/Shark-attack.py
+++ Shark-attack/Shark-attack.py
@@ -5,12 +5,38 @@ from matplotlib import pyplot as plt
 from matplotlib import image as image
 from pprint import pprint
 
+def get_shark(I):
+    # convert to contrasty color space 
+    Y = cv2.cvtColor(I, cv2.COLOR_BGR2LUV)
+    G = cv2.cvtColor(Y, cv2.COLOR_BGR2GRAY)
+    eqs = cv2.equalizeHist(G)
+
+    # Get an apply threshold
+    T = np.mean(eqs) + np.std(eqs) - 15
+    T, B = cv2.threshold(eqs, thresh = T, maxval= 255,type = cv2.THRESH_BINARY)
+
+    return B
+
+def get_shark2(I):
+    # convert to contrasty color space 
+    Y = cv2.cvtColor(I, cv2.COLOR_BGR2HLS)
+    G = cv2.cvtColor(Y, cv2.COLOR_BGR2GRAY)
+    eqs = cv2.equalizeHist(G)
+
+    # Get an apply threshold
+    T = np.mean(eqs) + np.std(eqs)
+    T, B = cv2.threshold(eqs, thresh = T, maxval= 255,type = cv2.THRESH_BINARY)
+
+    return eqs
 
 #get image
 I = cv2.imread("Shark 1.PNG")
+B = get_shark(I)
+C = get_shark2(I)
 
-IbutYUV = cv2.cvtColor(I, cv2.COLOR_BGR2YUV)
-Ibut = cv2.cvtColor(I, cv2.COLOR_BGR2YUV_I420)
-
-plt.imshow(Ibut)
+# write out 
+plt.imshow(B, cmap="gray")
 plt.savefig("tst.png")
+
+plt.imshow(C, cmap="gray")
+plt.savefig("tst2.png")
